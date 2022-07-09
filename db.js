61dc790a5a8f87a58717436ab2f3703154537428
st-hieuhuynh <hieu.huynh@supremetech.vn>
2022-07-09 17:51:38 +0700

    update heroku.yml

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres' /* 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+  dialectOptions: {
+    ssl: true
+  },
   port: process.env.DB_PORT
 });
 
